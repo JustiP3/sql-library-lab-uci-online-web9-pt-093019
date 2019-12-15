2e06f390e4c9d0fe81ef006cc68cb47009915e7b
Justin Phillips <justinphil02@yahoo.com>
2019-12-15 00:43:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) AS species_count 
+  FROM characers 
+  ORDER BY species_count DESC 
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
