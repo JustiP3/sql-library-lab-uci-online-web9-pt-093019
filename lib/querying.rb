9548683d979e84ae593c3060f85e83b6215972c6
Justin Phillips <justinphil02@yahoo.com>
2019-12-15 00:56:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,13 +32,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title 
-  FROM series 
-  INNER JOIN authors 
-  ON series.author_id = authors.id 
-  INNER JOIN characters 
+  "SELECT series.title
+  FROM series
+  INNER JOIN authors
+  ON series.author_id = authors.id
+  INNER JOIN characters
   ON authors.id = characters.author_id
-  GROUP BY series.title 
+  GROUP BY series.title
   ORDER BY COUNT(characters.species='human') DESC LIMIT 1;"
 end
 
