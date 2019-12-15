69beb51ee33f13cfaf5e625cc3c1c5748e83b663
Justin Phillips <justinphil02@yahoo.com>
2019-12-15 00:29:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   INNER JOIN series
   ON books.series_id = series.id
   GROUP BY series.author_id
-  ORDER BY series.id ASC;"
+  ORDER BY books.year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
